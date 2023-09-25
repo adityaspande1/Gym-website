dbce0d0adbde9cf137b55f1861cafb09e1884db1
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2023-09-25 18:26:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,176 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
+});
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  duration: 0.5,
+  height: "110px",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from("#about-left", {
+  x:-100,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-left",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from("#about-right", {
+  x:100,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-right",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+// gsap.from(".card", {
+//   scale: 0,
+//   // opacity:0,
+//   duration: 1,
+//   stagger: 0.1,
+//   scrollTrigger: {
+//     trigger: ".card",
+//     scroller: "body",
+//     // markers:false,
+//     start: "top 70%",
+//     end: "top 65%",
+//     scrub: 1,
+//   },
+// });
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 100,
+  opacity:0,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
+gsap.from("#card1", {
+    x:-100,
+    opacity:0,
+    scrollTrigger: {
+      trigger: "#card1",
+      scroller: "body",
+    //   markers:true,
+      start: "top 75%",
+      end: "top 60%",
+      scrub: 3,
+    },
+  });
+  gsap.from("#card3", {
+    x:100,
+    opacity:0,
+    scrollTrigger: {
+      trigger: "#card3",
+      scroller: "body",
+    //   markers:true,
+      start: "top 75%",
+      end: "top 65%",
+      scrub: 2,
+    },
+  });
+  gsap.from("#card2", {
+    y:100,
+    opacity:0,
+    scrollTrigger: {
+      trigger: "#card2",
+      scroller: "body",
+    //   markers:true,
+      start: "top 75%",
+      end: "top 65%",
+      scrub: 2,
+    },
+  });
+  
